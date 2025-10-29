0969b1ec58e617d5e45dd4c3c1cdb447148f15b1
habib4445 <0124habibur@gmail.com>
2025-10-29 23:19:13 +0600

    main

---
+++ login.js
@@ -0,0 +1,20 @@
+document.getElementById("login-btn").addEventListener("click", 
+    function(event){
+        event.preventDefault();
+    const accountNumber=document.getElementById('Account-number').value ;
+    const pin=document.getElementById('pin').value;
+    const convertedPin=parseInt(pin);
+
+    if(accountNumber.length<=11){
+      if(convertedPin===1234){
+window.location.href="./main.html";
+      }
+      else{
+        alert("pin thik nai");
+      }
+    }
+    else{
+        alert('invalid');
+    }
+}
+)
